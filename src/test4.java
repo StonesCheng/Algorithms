301045160041b4023fde77847d1f1cedd76a3de1
freedom <1217357099@qq.com>
2017-08-29 19:26:56 +0800

    几种基本排序算法
    Signed-off-by: freedem <1217357099@qq.com>
    Signed-off-by: freedom <1217357099@qq.com>

--- src/test4.java
+++ src/test4.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
  * Created by RayCheng on 2017/8/28.
  */
 public class test4 {
-    public class TreeNode {
+    public static class TreeNode {
         int val;
         TreeNode left;
         TreeNode right;
@@ -23,8 +23,34 @@ public class test4 {
         for(int i=0;i<b.length;i++)
             in[i]=Integer.valueOf(b[i]).intValue();
         TreeNode test=reConstructBinaryTree(pre,in);
+        print(test);
     }
     public static TreeNode reConstructBinaryTree(int [] pre, int [] in) {
+        TreeNode root=reConstructBinaryTree(pre,0,pre.length-1,in,0,in.length-1);
+        return root;
+    }
+
+    private static  TreeNode reConstructBinaryTree(int [] pre,int startPre,int endPre,int [] in,int startIn,int endIn) {
+        if(startPre>endPre||startIn>endIn)
+            return null;
+        TreeNode root=new TreeNode(pre[startPre]);
+        for(int i=startIn;i<=endIn;i++)
+        if(in[i]==pre[startPre]){
+            root.left=reConstructBinaryTree(pre,startPre+1,startPre+i-startIn,in,startIn,i-1);
+            root.right=reConstructBinaryTree(pre,i-startIn+startPre+1,endPre,in,i+1,endIn);
+        }
+        return root;
+}
+    public static void print(TreeNode test){
+        if(test==null)
+            System.out.println();
+        else
+            System.out.println(test.val);
+        if (test.right != null)
+            print(test.right);
+        if (test.left != null)
+            print(test.left);
+
 
     }
 }
---
+++ src/test5.java
@@ -0,0 +1,22 @@
+import java.util.Stack;
+
+public class test5 {
+    Stack<Integer> stack1=new Stack<>();
+    Stack<Integer> stack2=new Stack<>();
+    int length=0;
+    public void push(int node){
+        stack1.push(node);
+    }
+    public int pop(){
+        while (!stack2.isEmpty()){
+            return stack2.pop();
+        }
+        while(!stack1.isEmpty()){
+            stack2.push(stack1.pop());
+        }
+        return stack2.pop();
+    }
+    public static void main(String[] args) {
+
+    }
+}
