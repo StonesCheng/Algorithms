5e31843b03404036bf083fdf7d5e8276edcad651
freedem <1217357099@qq.com>
2017-09-01 17:05:32 +0800

    剑指Offer
    Signed-off-by: freedem <1217357099@qq.com>

---
+++ src/test13.java
@@ -0,0 +1,23 @@
+/**
+ * Created by RayCheng on 2017/9/1.
+ */
+public class test13 {
+    public static void main(String[] args) {
+        int [] test={2,4,6,1,3,5,7};
+        reOrderArray(test);
+        for(int i=0;i<test.length;i++)
+            System.out.println(test[i]);
+    }
+    public static void reOrderArray(int [] array) {
+        int[] temp=new int[array.length];
+        int a=0;int b=array.length-1;
+        for(int i=0,j=array.length-1;i<array.length&j>=0;i++,j--){
+            if(array[i]%2==1)
+                temp[a++]=array[i];
+            if(array[j]%2==0)
+                temp[b--]=array[j];
+        }
+        for(int i=0;i<array.length;i++)
+            array[i]=temp[i];
+    }
+}
---
+++ src/test14.java
@@ -0,0 +1,18 @@
+/**
+ * Created by RayCheng on 2017/9/1.
+ */
+public class test14 {
+    public class ListNode {
+    int val;
+    ListNode next = null;
+    ListNode(int val) {
+        this.val = val;
+    }
+    }
+    public static void main(String[] args) {
+
+    }
+    public ListNode FindKthToTail(ListNode head,int k) {
+
+    }
+}
