e3f533e0d382d47be3dfffae371cd04f1ab5f307
freedem <1217357099@qq.com>
2017-09-04 19:17:11 +0800

    剑指Offer
    Signed-off-by: freedem <1217357099@qq.com>

--- src/test16.java
+++ src/test16.java
@@ -13,6 +13,27 @@ public class test16 {
 
     }
     public static ListNode Merge(ListNode list1,ListNode list2) {
+        ListNode head=null;
+        ListNode current=null;
+        if(list1==null&&list2==null) return null;
+        if(list1==null&&list2!=null) return list2;
+        if(list1!=null&&list2==null) return list1;
+        if(list1.val>list2.val) {
+            head = list2;
+            current=list2.next;
+        }
+        else {
+            head = list1;
+            current=list1.next;
+        }
+        while(list1.next!=null|list2.next!=null){
+            if(list1.next.val>list2.next.val){
 
+            }
+            else{
+
+            }
+
+        }
     }
 }
