99456d78fb036deac31facb6c64687ddd7f6a062
RayCheng <1217357099@qq.com>
2017-09-26 19:11:16 +0800

    剑指Offer
    Signed-off-by: RayCheng <1217357099@qq.com>

--- src/test.java
+++ src/test.java
@@ -1,29 +1,41 @@
-import java.util.*;
-
 /**
  * Created by RayCheng on 2017/9/12.
  */
 public class test {
+    public static class ListNode {
+        int val;
+        ListNode next = null;
+        ListNode(int val) {
+            this.val = val;
+        }
+    }
+
     public static void main(String[] args) {
-        Scanner scanner=new Scanner(System.in);
-        int n=scanner.nextInt();
-        int[] temp=new int[n];
-        HashSet<Integer> set=new HashSet<>();
-        for(int i=0;i<n;i++) {
-            temp[i] = scanner.nextInt();
-            set.add(temp[i]);
+        ListNode list1=new ListNode(1);
+        ListNode list2=new ListNode(2);
+        list1.next=new ListNode(3);
+        list1.next.next=new ListNode(5);
+        list2.next=new ListNode(4);
+        list2.next.next=new ListNode(6);
+        ListNode test= Merge(list1,list2);
+        printf(test);
+    }
+    public static ListNode Merge(ListNode list1, ListNode list2) {
+        if(list1==null)
+            return list2;
+        if(list2==null)
+            return list1;
+        if(list1.val<=list2.val){
+            list1.next=Merge(list1.next,list2);
+            return list1;
+        }else {
+            list2.next=Merge(list1,list2.next);
+            return list2;
         }
-        Integer[] temp1=set.toArray(new Integer[] {});
-        int[] temp2=new int[temp1.length];
-        for(int i=0;i<temp1.length;i++) {
-            temp2[i] = temp1[i].intValue();
-        }
-        Arrays.sort(temp2);
-        Map<Integer,Integer> map=new HashMap<>();
-        for(int i=0;i<temp2.length;i++)
-            map.put(temp2[i],i+1);
-        for(int i=0;i<n-1;i++)
-            System.out.print(map.get(temp[i])+" ");
-        System.out.println(map.get(temp[n-1]));
+    }
+    public static void printf(ListNode in){
+        System.out.println(in.val);
+        if(in.next!=null)
+            printf(in.next);
     }
 }
